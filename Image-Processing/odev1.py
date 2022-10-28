a3da32394728a0c131cfcd9a9109226b208b118e
Bahadır Osman Doğan <osmandogan245@gmail.com>
2022-10-28 17:45:52 +0300

    histogram calculation

---
+++ Image-Processing/odev1.py
@@ -0,0 +1,19 @@
+import cv2
+import matplotlib.pyplot as plt
+
+foto = cv2.imread("kopek.jpeg",0)
+h = [0] * 256
+for x in range(foto.shape[0]):
+    for y in range(foto.shape[1]):
+        i = foto[x, y]
+        h[i] = h[i] + 1
+
+x = []
+for i in range(0,256):
+    x.append(i)
+
+plt.stem(x, h)
+plt.xlabel('Renk Tonları')
+plt.ylabel('Piksel Sayısı')
+plt.title('Görüntünün Histogramı')
+plt.show()
