ee7bf8579c6d8398a29d9a178b9ce4b7b2d1d99c
Bahadır Osman Doğan <85416387+bahadrdg@users.noreply.github.com>
2022-10-28 20:13:34 +0300

    Update histogram.py

--- histogram calculation/histogram.py
+++ histogram calculation/histogram.py
@@ -1,9 +1,9 @@
 import cv2
 import matplotlib.pyplot as plt
 
-foto = cv2.imread("kopek.jpeg",0)
+foto = cv2.imread("kopek.jpeg",0) 
 h = [0] * 256
-for x in range(foto.shape[0]):
+for x in range(foto.shape[0]):      #histogram hesaplaması yapıyoruz
     for y in range(foto.shape[1]):
         i = foto[x, y]
         h[i] = h[i] + 1
